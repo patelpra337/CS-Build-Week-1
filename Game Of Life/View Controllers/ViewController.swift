06ecee93a97e4f9dcf3e658bceb4813c658eef39
Pravin Patel <patelpra337@gmail.com>
2020-10-14 21:43:05 -0500

    Add new project GameOfLife, Create Storyboard, Organize project

---
+++ Game Of Life/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game Of Life
+//
+//  Created by patelpra on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
